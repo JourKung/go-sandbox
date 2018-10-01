85668fc4c37814e427504c8e9e3506a267f90528
Pichet Itngam <pichet.i@linecorp.com>
2018-10-01 11:04:12 +0700

    Test

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello")
+}
+
+// Hello to the world
+func Hello() string {
+	return "Hello, World"
+}
+
+// Hello2 to the world two
+func Hello2() string {
+	return "Hello, World 2"
+}
